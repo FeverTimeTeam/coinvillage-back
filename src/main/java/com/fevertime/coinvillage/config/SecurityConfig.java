0e322c1956631d46ea0ff3cfa30762cc724bd4af
jmk307 <jimin112688@gmail.com>
2022-09-02 16:04:27 +0900

    feat : 스웨거 세팅

---
+++ src/main/java/com/fevertime/coinvillage/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.fevertime.coinvillage.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring()
+                .antMatchers(
+                        "/h2-console/**"
+                        ,"/favicon.ico"
+                        ,"/error"
+                );
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+
+                .exceptionHandling()
+
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                // swagger 401 issue
+                .antMatchers("/").anonymous()
+                .antMatchers("/swagger-ui-html").anonymous()
+                .antMatchers("/webjars/**").anonymous()
+                .antMatchers("/swagger-resources/**").anonymous()
+                .antMatchers("/v2/**").anonymous()
+                .antMatchers("/csrf").anonymous()
+
+                .anyRequest().permitAll()
+
+                .and();
+        return http.build();
+    }
+}
---
+++ src/main/java/com/fevertime/coinvillage/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.fevertime.coinvillage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.fevertime.coinvillage.controller.api"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("KB 국민은행 공모전 - 코빌")
+                .description("코빌 REST API")
+                .version("0.1.0")
+                .build();
+    }
+}
