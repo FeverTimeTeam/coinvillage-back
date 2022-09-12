695d0df89d9556136232aff2d01fcc684e660f0e
jmk307 <jimin112688@gmail.com>
2022-09-12 18:51:56 +0900

    Update BackendApplication.java

--- src/main/java/com/fevertime/coinvillage/BackendApplication.java
+++ src/main/java/com/fevertime/coinvillage/BackendApplication.java
@@ -10,7 +10,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableScheduling
 public class BackendApplication extends SpringBootServletInitializer {
 
     // ec2 404에러 해결 코드
